bd54c86d2b72ba598cb0e8a45f907f363020ff4f
Eugenio Rovira <eugenio.rovirax@gmail.com>
2023-02-05 01:34:10 -0300

    Makes script POSIX compliant
    Bashisms were removed, and the script should run without errors on the
    dash shell on ubuntu.
    
    `shellcheck --shell sh` doesn't show any issues as well.

--- transa.sh
+++ transa.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Make the shell script more safe
 # - errexit: Stops execution whenever a command fails.
@@ -29,7 +29,7 @@ if [ $# -eq 0 ]; then
     EXCHANGE=$DEFAULT
 elif [ $# -eq 1 ]; then
     # transa <number> or transa brou
-    if [[ $1 =~ ^[$]?[0-9]*\.?[0-9]+$ ]]; then
+    if echo "$1" | grep -E -q '^[$]?[[:digit:]]*\.?[[:digit:]]+$'; then
         AMOUNT=$1
         EXCHANGE=$DEFAULT
     else
@@ -46,13 +46,13 @@ fi
 # Default to "itau" and convert to lowercase
 EXCHANGE=$(echo "$EXCHANGE" | tr '[:upper:]' '[:lower:]')
 
-if [[ $EXCHANGE == "brou" ]]; then
+if [ "$EXCHANGE" = "brou" ]; then
     EXCHANGE_NAME="eBROU"
     html_contents=$(wget -qO- 'https://www.brou.com.uy/c/portal/render_portlet?p_l_id=20593&p_p_id=cotizacionfull_WAR_broutmfportlet_INSTANCE_otHfewh1klyS&p_p_lifecycle=0&p_t_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-1&p_p_col_pos=0&p_p_col_count=2&p_p_isolated=1&currentURL=%2Fweb%2Fguest%2Fcotizaciones')
     RATES=$(echo "$html_contents" | xmllint --html --xpath "//p[text()='DÃ³lar eBROU']/../../..//p[@class='valor']/text()" - | tr ',' '.' | tr '\n' ' ')
     BUY=$(echo "$RATES" | awk '{print $1}')
     SELL=$(echo "$RATES" | awk '{print $2}')
-elif [[ $EXCHANGE == "itau" ]]; then
+elif [ "$EXCHANGE" = "itau" ]; then
     EXCHANGE_NAME="Itaú"
     xml_contents=$(wget -qO- https://www.itau.com.uy/inst/aci/cotiz.xml)
     BUY=$(echo "$xml_contents" | xmllint --xpath "//cotizacion[moneda='LINK']/compra/text()" - | tr ',' '.')
@@ -79,23 +79,21 @@ echo "${MEAN_EXPR} = ${MEAN}"
 beginswith() { case "$2" in "$1"*) true ;; *) false ;; esac }
 
 # Use GNU's printf instead of bash builtin
+# `printf %.2f` rounds the input to two decimal places.
 ROUND_TWO="/usr/bin/printf %.2f"
 
-if [[ -n "$AMOUNT" ]]; then
-    # `printf %.2f` rounds the input to two decimal places.
-
+if [ "$AMOUNT" != "" ]; then
     if beginswith "$" "$AMOUNT"; then
         # The `cut` command drops the initial `$` from the input.
-        UYU=$(echo "$($ROUND_TWO "$(echo "$AMOUNT" | cut -c 2-)")" | tr ',' '.')
-        # echo "UYU ES $UYU"
+        AMOUNT=$(echo "$AMOUNT" | cut -c 2-)
+        UYU=$($ROUND_TWO "$AMOUNT" | tr ',' '.')
         USD=$($ROUND_TWO "$(echo "(${UYU} / ${MEAN})" | $CALC)")
     else
-        USD=$(echo $($ROUND_TWO "$AMOUNT") | tr ',' '.')
+        USD=$($ROUND_TWO "$AMOUNT" | tr ',' '.')
         UYU=$($ROUND_TWO "$(echo "(${USD} * ${MEAN})" | $CALC)") # This fails with bash's builtin printf
     fi
 
-    if [ "$USD" != "0.00" ]; then
-        echo ""
-        echo "TRANSA: U\$S $USD = $ $UYU"
-    fi
+    echo ""
+    echo "TRANSA: U\$S $USD = $ $UYU"
+
 fi
