f1d09300bddccfc2a49cd06b326dffff469a7cc9
Eugenio Rovira <eugenio.rovirax@gmail.com>
2023-02-04 23:27:28 -0300

    Small changes to make it functional on ubuntu

--- transa.sh
+++ transa.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Make the shell script more safe
 # - errexit: Stops execution whenever a command fails.
@@ -25,7 +25,7 @@ exit_instructions() {
 
 # Act depending on the number of arguments
 if [ $# -eq 0 ]; then
-    AMOUNT=0
+    AMOUNT=""
     EXCHANGE=$DEFAULT
 elif [ $# -eq 1 ]; then
     # transa <number> or transa brou
@@ -33,7 +33,7 @@ elif [ $# -eq 1 ]; then
         AMOUNT=$1
         EXCHANGE=$DEFAULT
     else
-        AMOUNT=0
+        AMOUNT=""
         EXCHANGE=$1
     fi
 elif [ $# -eq 2 ]; then
@@ -44,14 +44,14 @@ else
 fi
 
 # Default to "itau" and convert to lowercase
-EXCHANGE=$(echo $EXCHANGE | tr '[:upper:]' '[:lower:]')
+EXCHANGE=$(echo "$EXCHANGE" | tr '[:upper:]' '[:lower:]')
 
 if [[ $EXCHANGE == "brou" ]]; then
     EXCHANGE_NAME="eBROU"
     html_contents=$(wget -qO- 'https://www.brou.com.uy/c/portal/render_portlet?p_l_id=20593&p_p_id=cotizacionfull_WAR_broutmfportlet_INSTANCE_otHfewh1klyS&p_p_lifecycle=0&p_t_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-1&p_p_col_pos=0&p_p_col_count=2&p_p_isolated=1&currentURL=%2Fweb%2Fguest%2Fcotizaciones')
-    RATES=$(echo "$html_contents" | xmllint --html --xpath "//p[text()='DÃ³lar eBROU']/../../..//p[@class='valor']/text()" - | tr ',' '.')
-    BUY=$(echo $RATES | awk '{print $1}')
-    SELL=$(echo $RATES | awk '{print $2}')
+    RATES=$(echo "$html_contents" | xmllint --html --xpath "//p[text()='DÃ³lar eBROU']/../../..//p[@class='valor']/text()" - | tr ',' '.' | tr '\n' ' ')
+    BUY=$(echo "$RATES" | awk '{print $1}')
+    SELL=$(echo "$RATES" | awk '{print $2}')
 elif [[ $EXCHANGE == "itau" ]]; then
     EXCHANGE_NAME="Itaú"
     xml_contents=$(wget -qO- https://www.itau.com.uy/inst/aci/cotiz.xml)
@@ -78,23 +78,24 @@ echo "${MEAN_EXPR} = ${MEAN}"
 # Thanks SO
 beginswith() { case "$2" in "$1"*) true ;; *) false ;; esac }
 
-ROUND_TWO="printf %.2f"
+# Use GNU's printf instead of bash builtin
+ROUND_TWO="/usr/bin/printf %.2f"
 
-if [ -n "$AMOUNT" ]; then
+if [[ -n "$AMOUNT" ]]; then
     # `printf %.2f` rounds the input to two decimal places.
 
     if beginswith "$" "$AMOUNT"; then
         # The `cut` command drops the initial `$` from the input.
-        UYU=$($ROUND_TWO "$(echo "$AMOUNT" | cut -c 2-)")
+        UYU=$(echo "$($ROUND_TWO "$(echo "$AMOUNT" | cut -c 2-)")" | tr ',' '.')
+        # echo "UYU ES $UYU"
         USD=$($ROUND_TWO "$(echo "(${UYU} / ${MEAN})" | $CALC)")
     else
-        USD=$($ROUND_TWO "$AMOUNT")
-        UYU=$($ROUND_TWO "$(echo "(${USD} * ${MEAN})" | $CALC)")
+        USD=$(echo $($ROUND_TWO "$AMOUNT") | tr ',' '.')
+        UYU=$($ROUND_TWO "$(echo "(${USD} * ${MEAN})" | $CALC)") # This fails with bash's builtin printf
     fi
 
     if [ "$USD" != "0.00" ]; then
         echo ""
         echo "TRANSA: U\$S $USD = $ $UYU"
     fi
-
 fi
